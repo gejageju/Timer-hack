0049bb2ffc508c189beb14abedcd499f72d8ca5c
gejageju <65595358+gejageju@users.noreply.github.com>
2022-01-12 23:21:39 +0530

    Update encrypter.py

--- Encrypter/encrypter.py
+++ Encrypter/encrypter.py
@@ -6,36 +6,33 @@ fernet = Fernet(key)
 
 def encrypt(site, pw):
     encMessage = fernet.encrypt(pw.encode())
-    file=open("site.txt",'w')
-    file.write(site)
-    file.write("\n")
-    file=open("password.txt",'wb')
+    file=open("Data/"+site+".txt",'wb')
     file.write(encMessage)
     print("Encrypted password successfully!")
+
 def decrypt(site):
-    with open("password.txt", "rb") as file:
-       count=1
+    with open("Data/"+site+".txt", "rb") as file:
        data = file.readlines()
        for line in data:
         print(fernet.decrypt(line).decode())
-        
-
                
 while(run):
     print("~~~~~Password Encrypter~~~~~~~~")
     print("Enter your choice")
-    x=int(input("1.Add password \n 2.View password \n"))
+    x=int(input("1.Add password \n2.View password \n"))
     if(x==1):
         site=input("Enter Site: ")
-        pw=input("Enter password")
+        pw=input("Enter password: ")
         encrypt(site,pw)
     else:
         if(x==2):
             site=input("Enter Site: ")
             decrypt(site)
             
-        else:
-            break
+    v=input("Do you want to continue y/n : ")
+    if(v!="y"):
+        break
+
 
 
 
